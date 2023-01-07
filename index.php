515d69de8932d09744176e549bd1ca1f22ebd677
Julibe <mail@julibe.com>
2023-01-07 15:14:50 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<h1>Phixel Backup 🌎</h1>
+<p>This repository contains a Markdown, HTML and XML backup of the <a href="http://phixel.net">http://phixel.net</a> website. The backup was created to ensure that the site's content is preserved and can be restored if needed.</p>
+<h2>About Phixel</h2>
+<p>Phixel is a team of specialized designers and developers with experience in personalized and unique digital solutions. We have worked with some well-known brands and are dedicated to helping our clients shape their ideas and bring them to life online.</p>
+<h2>Contents of the Backup</h2>
+<p>The backup in this repository includes Markdown and XML files containing the content of the Phixel.net website. These files can be used to restore the website if necessary.</p>
+<h2>Using the Backup</h2>
+<p>Instructions for using the backup files to restore the website will be provided in the repository.</p>
+<h2>Contact</h2>
+<p>If you have any questions or need assistance with the backup files, please don't hesitate to contact us at <a href="contact@phixel.net">contact@phixel.net</a>.</p>
