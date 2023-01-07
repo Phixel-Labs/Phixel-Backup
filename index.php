370745387417dfefc3d1a2680160cdcfff16c38a
Julibe <mail@julibe.com>
2023-01-07 15:20:19 -0500

    Phixel Website Backup

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <h2>About Phixel</h2>
 <p>Phixel is a team of specialized designers and developers with experience in personalized and unique digital solutions. We have worked with some well-known brands and are dedicated to helping our clients shape their ideas and bring them to life online.</p>
 <h2>Contents of the Backup</h2>
-<p>The backup in this repository includes Markdown and XML files containing the content of the Phixel.net website. These files can be used to restore the website if necessary.</p>
+<p>This repository contains a backup of the Markdown, HTML, and XML files for the Phixel website, as well as any associated media such as videos, audio, 3D objects, and images. The backup was created to ensure that the site's content is preserved and can be restored if needed. </p>
 <h2>Using the Backup</h2>
 <p>Instructions for using the backup files to restore the website will be provided in the repository.</p>
 <h2>Contact</h2>
